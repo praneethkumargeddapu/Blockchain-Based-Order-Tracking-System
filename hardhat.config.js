45f489c79f08189439a52e5b899dbeae6446265d
Praneeth Kumar Geddapu <pgeddap1@asu.edu>
2026-04-30 22:31:00 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-
-module.exports = {
-  solidity: "0.8.20",
-  networks: {
+module.exports ={
+  solidity:"0.8.20",
+  networks:{
     hardhat: {},
-    amoy: {
-      url: process.env.AMOY_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-      chainId: 80002,
-    },
+    amoy:{
+    url: process.env.AMOY_RPC_URL,
+    accounts: [process.env.PRIVATE_KEY],
+    chainId: 80002},
   },
 };
