c8538de77ca2d3c63fa174809badce7d6243ee89
Praneeth Kumar Geddapu <pgeddap1@asu.edu>
2026-04-30 22:34:08 -0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,18 @@
-const { ethers } = require("hardhat");
-
-async function main() {
+const {ethers} =require("hardhat");
+async function main(){
   const [deployer] = await ethers.getSigners();
   console.log("Deploying with account:", deployer.address);
-
   const balance = await ethers.provider.getBalance(deployer.address);
   console.log("Balance:", ethers.formatEther(balance));
-
   const OrderTracking = await ethers.getContractFactory("OrderTracking");
   const contract = await OrderTracking.deploy();
   await contract.waitForDeployment();
-
   const address = await contract.getAddress();
   console.log("Contract deployed to:", address);
 }
-
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+.then(() => process.exit(0))
+.catch((error) =>{
+  console.error(error);
+  process.exit(1);
+});
